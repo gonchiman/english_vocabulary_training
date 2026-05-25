c776a3cf80864b948cd4374426c4d9bc6c19b2b4
gonchiman <legendaryyamasama@gmail.com>
2026-05-25 23:07:36 +0900

    past_of_speech

---
+++ src/constants/part_of_speech.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class PartOfSpeech(Enum):
+    NOUN = "Noun"
+    VERB = "Verb"
+    ADJECTIVE = "Adjective"
+    ADVERB = "Adverb"
+    PRONOUN = "Pronoun"
+    PREPOSITION = "Preposition"
+    CONJUNCTION = "Conjunction"
+    INTERJECTION = "Interjection"
