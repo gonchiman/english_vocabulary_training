9c45a47e65cf6638176fae5bb28e4859d581b8ff
gonchiman <legendaryyamasama@gmail.com>
2026-05-28 11:18:33 +0900

    upd

--- app.py
+++ app.py
@@ -51,17 +51,6 @@ def parents_registration():
     )
 
 
-@app.route('/edit', methods=['GET', 'POST'])
-def edit():
-    return render_template(
-        'edit.html',
-        page_title='Edit',
-        parts_of_speech=PartsOfSpeech,
-        words=[parent["word"] for parent in ParentRepository.find_by_condition("part_of_speech", PartsOfSpeech.NOUN.value)],
-        children=[]
-    )
-
-
 @app.route('/children_registration', methods=['GET', 'POST'])
 def children_registration():
     input_word = request.form.get("word")
@@ -83,19 +72,59 @@ def children_registration():
     )
 
 
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    return render_template(
+        'edit.html',
+        page_title='Edit',
+        parts_of_speech=PartsOfSpeech,
+        parents=[parent["word"] for parent in ParentRepository.find_by_condition("part_of_speech", PartsOfSpeech.NOUN.value)],
+        children=[]
+    )
+    
+
 @app.route('/show_children_list', methods=['GET', 'POST'])
 def show_children_list():
-    selected_part_of_speech = request.form.get("part_of_speech", default=PartsOfSpeech.NOUN.value)
-    selected_word = request.form.get("word")
+    selected_part_of_speech = request.form.get(
+        "part_of_speech",
+        default=PartsOfSpeech.NOUN.value
+    )
+
+    parents = [
+        parent["word"]
+        for parent in ParentRepository.find_by_condition(
+            "part_of_speech",
+            selected_part_of_speech
+        )
+    ]
+
+    selected_parent = request.form.get("parent")
+
+    if selected_parent not in parents:
+        if parents:
+            selected_parent = parents[0]
+        else:
+            selected_parent = None
+
+    if selected_parent is None:
+        children = []
+    else:
+        children = [
+            child["word"]
+            for child in ChildrenRepository.find_by_condition(
+                "parent",
+                selected_parent
+            )
+        ]
 
     return render_template(
         'edit.html',
         page_title='Edit',
         selected_part_of_speech=selected_part_of_speech,
-        selected_word=selected_word,
+        selected_parent=selected_parent,
         parts_of_speech=PartsOfSpeech,
-        words=[parent["word"] for parent in ParentRepository.find_by_condition("part_of_speech", selected_part_of_speech)],
-        children=[child["word"] for child in ChildrenRepository.find_by_condition("parent", selected_word)]
+        parents=parents,
+        children=children
     )
 
 
